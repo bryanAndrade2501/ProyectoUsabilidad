1b428381739ed135f356b22c461002bed84576fe
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2023-09-02 09:13:42 -0500

    A

--- js/menu_principal.js
+++ js/menu_principal.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Tu código JavaScript aquí
+import products from "../providers/productProvider.js";
 
+document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".prevBtn").addEventListener("click", prevSlide);
   document.querySelector(".nextBtn").addEventListener("click", nextSlide);
   const sliderImg = document.querySelector("#sliderImg");
@@ -11,10 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "/assets/images/metodos_de_pago.jpg",
   ];
 
-  const description = [
-    "promociones de celulares",
-    "metodos de paog"
-  ]
+  const description = ["promociones de celulares", "metodos de pago"];
 
   function nextSlide() {
     slideIndex++;
@@ -22,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
       slideIndex = 0; // Reiniciar al llegar al final
     }
     sliderImg.setAttribute("src", images[slideIndex]);
-    sliderImg.setAttribute("alt", `Imagen de ${description[slideIndex]}`)
+    sliderImg.setAttribute("alt", `Imagen de ${description[slideIndex]}`);
   }
 
   function prevSlide() {
@@ -31,7 +28,48 @@ document.addEventListener("DOMContentLoaded", function () {
       slideIndex = images.length - 1; // Ir al último al retroceder desde el primero
     }
     sliderImg.setAttribute("src", images[slideIndex]);
-    sliderImg.setAttribute("alt", `Imagen de ${description[slideIndex]}`)
+    sliderImg.setAttribute("alt", `Imagen de ${description[slideIndex]}`);
   }
   setInterval(nextSlide, 3000);
 });
+
+//CARGA DINÁMICA DE PRODUCTOS
+
+function crearProducto(producto) {
+  const productoHTML = `
+      <div class="product">
+          <img src="${producto.image}" alt="${producto.productName}" />
+          <h3>${producto.productName}</h3>
+          <p>Precio: $${producto.price}</p>
+          <div>
+          <button class="btn-cart">Añadir al carrito</button>
+          <button class="btn-favorite">❤️</button>
+          </div>
+      </div>
+  `;
+  return productoHTML;
+}
+
+// Obtener los primeros 5 productos
+const celulares = products.slice(0, 5);
+
+// Obtener los siguientes 5 productos (del 6 al 10)
+const ultimos = products.slice(8, 13);
+
+function cargarProductos() {
+  const cellphone_container = document.querySelector(".cell_phone");
+  const userpreferences_container = document.querySelector(".user_preferences")
+  
+
+  celulares.forEach((producto) => {
+    const productoHTML = crearProducto(producto);
+    cellphone_container.innerHTML += productoHTML;
+  });
+
+  ultimos.forEach((producto) => {
+    const productoHTML = crearProducto(producto);
+    userpreferences_container.innerHTML += productoHTML;
+  });
+}
+
+cargarProductos();
